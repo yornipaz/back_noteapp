49edf7c69104f5cadf51714f076605b9f29fe59c
felipebonilla <ybonilla@serempre.com>
2023-03-04 17:47:42 -0500

    Fix: Origin url app core

--- middleware/core.go
+++ middleware/core.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200,https://guatic.netlify.app")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://guatic.netlify.app")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization, Cache-Control")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT,PATCH,DELETE")
