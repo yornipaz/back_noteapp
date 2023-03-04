d6fdd34377d1b52ed6fb7b646b3e8cc9d0b09789
felipebonilla <ybonilla@serempre.com>
2023-03-04 15:38:15 -0500

    Feat:Path for env

--- config/variableConfig.go
+++ config/variableConfig.go
@@ -7,7 +7,7 @@ import (
 )
 
 func loadVariables() {
-	err := godotenv.Load("nix-env.env")
+	err := godotenv.Load("../nix-env.env")
 
 	if err != nil {
 		log.Fatal("Error loading .env file ", err.Error())
