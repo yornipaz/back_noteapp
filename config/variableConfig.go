1fbb24fb40210c1388337dab1982f5ea48e9c774
felipebonilla <ybonilla@serempre.com>
2023-03-04 15:36:49 -0500

    Refactor: Refactor load variable

--- config/variableConfig.go
+++ config/variableConfig.go
@@ -1,26 +1,14 @@
 package config
 
 import (
-	"fmt"
 	"log"
-	"os/exec"
 
 	"github.com/joho/godotenv"
 )
 
 func loadVariables() {
 	err := godotenv.Load("nix-env.env")
-	fmt.Println("load variables")
-	out, errCd := exec.Command("cd out/").Output()
-	if errCd != nil {
-		log.Fatal(errCd)
-	}
-	fmt.Println(string(out))
-	ls, errLs := exec.Command("cd out/").Output()
-	if errLs != nil {
-		log.Fatal(errLs)
-	}
-	fmt.Println(string(ls))
+
 	if err != nil {
 		log.Fatal("Error loading .env file ", err.Error())
 	}
