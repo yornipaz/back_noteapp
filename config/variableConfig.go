8120e7419e5e2b7138f0dc84de231606748f83ae
felipebonilla <ybonilla@serempre.com>
2023-03-04 14:26:43 -0500

    Feat:Add  load variables

--- config/variableConfig.go
+++ config/variableConfig.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"log"
+	"os/exec"
 
 	"github.com/joho/godotenv"
 )
@@ -10,6 +11,11 @@ import (
 func loadVariables() {
 	err := godotenv.Load("nix-env.env")
 	fmt.Println("load variables")
+	out, errExec := exec.Command("ls").Output()
+	if errExec != nil {
+		log.Fatal(errExec)
+	}
+	fmt.Println(string(out))
 	if err != nil {
 		log.Fatal("Error loading .env file ", err.Error())
 	}
